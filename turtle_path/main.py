34e5a1e436a0fa91cf70a3085bda812e4250f03c
ouhoy <kimkinon@gmail.com>
2023-03-20 12:53:44 +0000

    Finish path looking code turtle

--- turtle_path/main.py
+++ turtle_path/main.py
@@ -1,5 +1,44 @@
-from turtle import *
+from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=750, height=500)
+FONT = ("Courier", 16, "normal")
 
+cinema = Turtle()
+mosque = Turtle()
+home = Turtle()
+message = Turtle()
+message.hideturtle()
+
+cinema.color("red")
+mosque.color("green")
+
+cinema.shape("circle")
+mosque.shape("circle")
+
+mosque.penup()
+cinema.penup()
+
+cinema.goto(100, 100)
+mosque.goto(40, 50)
+
+home.color("blue")
+home.forward(40)
+home.left(90)
+home.forward(50)
+home.right(90)
+home.forward(60)
+home.left(90)
+home.forward(50)
+
+message.penup()
+message.goto(0, 200)
+message.pendown()
+
+if home.distance(cinema) < 40:
+    message.write(f"You are there!! ", align="center", font=FONT)
+
+else:
+    message.write(f"Oops you are not there yet!!", align="center", font=FONT)
+
+screen.exitonclick()
