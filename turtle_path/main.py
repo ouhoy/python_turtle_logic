e751130b3111bfe2b708ca859743f97e08891c1d
ouhoy <kimkinon@gmail.com>
2023-03-20 13:00:58 +0000

    update path find

--- turtle_path/main.py
+++ turtle_path/main.py
@@ -25,7 +25,12 @@ mosque.goto(40, 50)
 home.color("blue")
 home.forward(40)
 home.left(90)
-home.forward(50)
+
+while home.distance(mosque) >= 0:
+    home.forward(1)
+    if home.distance(mosque) == 0:
+        break
+
 home.right(90)
 home.forward(60)
 home.left(90)
