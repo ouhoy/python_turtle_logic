48aa5e1676ba5cd8efd4209a3baaac51d630d5d5
ouhoy <kimkinon@gmail.com>
2023-03-22 17:27:04 +0000

    move turtle

--- turtle_practice_session/main.py
+++ turtle_practice_session/main.py
@@ -6,4 +6,15 @@ screen.title("Turtle Practice Session")
 
 tim = Turtle()
 
+tim.pen(pencolor="red", fillcolor="green")
+tim.hideturtle()
+tim.begin_fill()
+tim.forward(200)
+tim.goto(0, 200)
+tim.home()
+tim.end_fill()
+
+screen.listen()
+screen.onkey(tim.undo, "Up")
+
 screen.exitonclick()
