8ef27fffcc410f2badaf8a28b507e5c649df5f84
ouhoy <kimkinon@gmail.com>
2023-03-23 10:53:22 +0000

    Improve code

--- morocco_flag/main.py
+++ morocco_flag/main.py
@@ -6,29 +6,38 @@ screen.setup(width=750, height=500)
 flag_bg = Turtle()
 star = Turtle()
 
-flag_bg.penup()
-flag_bg.goto(-200, -120)
-flag_bg.pendown()
+star.hideturtle()
 
 
-def draw_square(linecolor, length=200):
+def draw_square(linecolor, width=200, height=100):
     flag_bg.color(linecolor)
+
+    flag_bg.penup()
+    flag_bg.goto(-150, -height)
+    flag_bg.pendown()
+
     for i in range(4):
-        flag_bg.forward(length)
+        flag_bg.forward(width)
         if i % 2 == 0:
-            flag_bg.forward(100)
+            flag_bg.forward(height)
         flag_bg.left(90)
+    flag_bg.hideturtle()
 
 
 flag_bg.begin_fill()
-draw_square('red')
+draw_square('red', )
 flag_bg.end_fill()
 
+star.showturtle()
 star.color("green")
 star.width(2)
+star.penup()
+star.goto(45, 45 / 2)
+star.pendown()
 
 for _ in range(5):
     star.right(144)
     star.forward(90)
+star.hideturtle()
 
 screen.exitonclick()
